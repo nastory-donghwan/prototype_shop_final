f0118968616d7866abf39925650895ecd2070721
welfare <welfare@welfareui-MacBookPro.local>
2023-06-05 00:20:58 +0900

    최종

--- src/App.js
+++ src/App.js
@@ -5,18 +5,19 @@ import Header from "./components/Header";
 import Prototypes from "./components/Prototypes";
 import Orders from "./components/Orders";
 import Footer from "./components/Footer";
+import AppStateProvider from "./providers/AppStateProvider";
 
 
 function App() {
   return (
-    <>
+    <AppStateProvider>
       <Header />
       <div className="container">
         <Prototypes />
         <Orders />
         <Footer />
       </div>
-    </>
+      </AppStateProvider>
 
     // <div className="App">
     //   <header className="App-header">
